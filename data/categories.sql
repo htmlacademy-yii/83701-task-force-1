68506e7cd62c56a46d1cb0b4db9e14777e9eb645
baradmitrij <baradmitrij@yandex.ru>
2020-01-23 23:56:00 +0200

    feat: add first version of ConvertorCsvToSql

---
+++ data/categories.sql
@@ -0,0 +1,9 @@
+INSERT INTO `tforce`.`categories` (`name`, `icon_link`) VALUES 
+ ('Курьерские услуги', 'translation'), 
+ ('Уборка', 'clean'), 
+ ('Переезды', 'cargo'), 
+ ('Компьютерная помощь', 'neo'), 
+ ('Ремонт квартирный', 'flat'), 
+ ('Ремонт техники', 'repair'), 
+ ('Красота', 'beauty'), 
+ ('Фото', 'photo'); 
--- web/index.php
+++ web/index.php
@@ -9,6 +9,7 @@ require_once '../vendor/autoload.php';
 use TForce\Logic\Task;
 use TForce\Actions\ActionCancel;
 
+/** TODO Check Task.php functionality
 $taskInstance = new Task(2, 3);
 
 $x1 = $taskInstance->getMapStatusAction();
@@ -22,6 +23,8 @@ $x7 = $taskInstance->getActionsByStatus(3,Task::STATUS_WORKING);
 echo "<br>" . __FILE__ . " --- " . __LINE__ . "<pre>";
 var_dump($x7);
 echo  "</pre><br>";
+*/
 
+/* TODO Check Parsing CSV files */
 
-
+include '../src/CsvToSql/ReadFilesFromStorage.php';
